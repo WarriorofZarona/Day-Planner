c131d3d860239cb95a1c3b5279ca7e5248117347
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-24 19:01:50 -0500

    created an array for the hours, created a function that makes the 9 timeblocks with appropriate times

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-//Create timeblocks
-
-//Create variables for elements
-
-// Targeting div container where code starts
-var targetContainer = $(".container");
-
-// Creating first row
-var createTimeBlockRow = $("<div>");
-createTimeBlockRow.addClass("row timeblock").attr("id", "row0");
-targetContainer.append(createTimeBlockRow);
-
-//Creating column for the row
-// Hour column
-var hourCol = $("<div>");
-hourCol.addClass("col-sm-1 hour description").text("9am");
-$("#row0").append(hourCol);
-
-// Event column
-var eventCol = $("<div>");
-eventCol.addClass("col-sm-10 textarea description past");
-$("#row0").append(eventCol);
-
-// Save column
-var saveCol = $("<div>");
-saveCol.addClass("col-sm-1 saveBtn").html("<i class=\"far fa-save\"></i>");
-$("#row0").append(saveCol);
-
 // Displaying current day
 var currentDay = $("#currentDay")
-currentDay.text(moment().format('dddd MMMM Do YYYY'));
+currentDay.text(moment().format('dddd, MMMM Do YYYY'));
+
+//Create timeblocks
+//Create variables for elements
+var targetContainer = $(".container");
+var hourArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+console.log(hourArr.length)
+Now going to try to turn it into a function
+
+    // Creates the 9am to 5pm timeblocks
+    createTimeBlock();
+
+function createTimeBlock() {
+    for (var i = 0; i < hourArr.length; i++) {
+        // Creating first row
+        var createTimeBlockRow = $("<div>");
+        createTimeBlockRow.addClass("row timeblock").attr("id", "row" + i);
+        targetContainer.append(createTimeBlockRow);
+        //Creating column for the row
+        // Hour column
+        var hourCol = $("<div>");
+        hourCol.addClass("col-sm-1 hour description").text(hourArr[i]);
+        $("#row" + i).append(hourCol);
+        // Event column
+        var eventCol = $("<div>");
+        eventCol.addClass("col-sm-10 textarea description past");
+        $("#row" + i).append(eventCol);
+        // Save column
+        var saveCol = $("<div>");
+        saveCol.addClass("col-sm-1 saveBtn").html("<i class=\"far fa-save\"></i>");
+        $("#row" + i).append(saveCol);
+    };
+};
 
 //In the timeblocks, there are 3 parts:
 // Display Hour, Display Time Event, Display Save Button
