7d7e625258d2b437bc6965be54d28845aa98e916
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-26 18:54:57 -0500

    removed console.logs and checked to make sure ; is at end of each line

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Displaying current day
-var currentDay = $("#currentDay")
-var displayCurrentDay = moment().format('dddd, MMMM Do YYYY')
+var currentDay = $("#currentDay");
+var displayCurrentDay = moment().format('dddd, MMMM Do YYYY');
 currentDay.text(displayCurrentDay);
 
 //Create variables for elements
@@ -22,18 +22,16 @@ checkTime();
 $(document).on("click", "button", function () {
     var userInput = $(this).parent().find("textarea").val();
     var textRow = $(this).parent().find("textarea").attr("data-row");
-    var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner"))
+    var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner"));
     var newEvent = new Object();
     newEvent.currentDay = displayCurrentDay;
-    newEvent.row = textRow
-    newEvent.text = userInput
+    newEvent.row = textRow;
+    newEvent.text = userInput;
     dayPlanner.unshift(newEvent);
-    console.log(dayPlanner);
-    dayPlanner = removeDuplicates(dayPlanner, "row")
+    dayPlanner = removeDuplicates(dayPlanner, "row");
     dayPlanner.sort(function (x, y) {
-        return x.row - y.row
+        return x.row - y.row;
     });
-    console.log(dayPlanner);
     localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
     // Adds a new entry and removes the old entry
     function removeDuplicates(array, key) {
@@ -43,18 +41,18 @@ $(document).on("click", "button", function () {
             if (!lookup[array[i][key]]) {
                 lookup[array[i][key]] = true;
                 result.push(array[i]);
-            }
-        }
+            };
+        };
         return result;
-    }
+    };
 });
 // Using moment.js, this creates an array of hours 9am to 5pm
 function createHourArr() {
     for (var i = 0; i < 9; i++) {
         var hour = moment().hour(i + 9).format("h a");
         hourArr.push(hour);
-    }
-}
+    };
+};
 
 // Using a for loop, this creates 9 time blocks displaying hour, clickable event text, and save buttons
 function createTimeBlock() {
@@ -96,25 +94,24 @@ function checkStorage() {
         for (var i = 0; i < hourArr.length; i++) {
             var generateObj = new Object();
             generateObj.currentDay = displayCurrentDay;
-            generateObj.row = i
-            generateObj.text = ""
+            generateObj.row = i;
+            generateObj.text = "";
             storage.push(generateObj);
             localStorage.setItem("dayPlanner", JSON.stringify(storage));
-        }
-    }
+        };
+    };
     localStorage.setItem("dayPlanner", JSON.stringify(storage));
-}
+};
 
 // This checks current time against hours 9am to 5pm and color codes time-blocks appropriately
 function checkTime() {
     for (var i = 0; i < hourArr.length; i++) {
-        console.log("Iteration " + i + ":");
         if (moment().isSame(moment().hour(9 + i))) {
             $("#text" + i).addClass("present");
         } else if (moment().isBefore(moment().hour(9 + i))) {
             $("#text" + i).addClass("future");
         } else {
             $("#text" + i).addClass("past");
-        }
-    }
-}
+        };
+    };
+};
