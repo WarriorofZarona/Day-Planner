4d43430bedad47afebb04817ea25c79859f9f0f7
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-26 00:39:18 -0500

    persisting text now works, need to make sure it matches current date

--- script.js
+++ script.js
@@ -6,10 +6,57 @@ currentDay.text(moment().format('dddd, MMMM Do YYYY'));
 //Create variables for elements
 var targetContainer = $(".container");
 var hourArr = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-var dayPlanner = [];
+
 
 // Creates the 9am to 5pm timeblocks
 createTimeBlock();
+
+checkStorage();
+
+
+renderEvents();
+
+$(document).on("click", "button", function () {
+    console.log("Clicking now. The day planner is the following:")
+    console.log(dayPlanner);
+    var userInput = $(this).parent().find("textarea").val();
+    var textRow = $(this).parent().find("textarea").attr("data-row");
+    var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner"))
+    console.log("The text inputed is " + userInput);
+
+    var newEvent = new Object();
+    newEvent.currentDay = $("#currentDay").text();
+    newEvent.row = textRow
+    newEvent.text = userInput
+    console.log("The new entry is " + newEvent);
+    dayPlanner.unshift(newEvent);
+    console.log("Entry added. Looks like this:")
+    console.log(dayPlanner);
+    dayPlanner = removeDuplicates(dayPlanner, "row")
+    console.log("Duplicates removed. Looks like this:")
+    console.log(dayPlanner);
+    dayPlanner.sort(function (x, y) {
+        return x.row - y.row
+    });
+    console.log("Objects sorted by row. Looks like this")
+    console.log(dayPlanner);
+
+    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+
+
+    function removeDuplicates(array, key) {
+        var lookup = {};
+        var result = [];
+        for (var i = 0; i < array.length; i++) {
+            if (!lookup[array[i][key]]) {
+                lookup[array[i][key]] = true;
+                result.push(array[i]);
+            }
+        }
+        return result;
+    }
+});
+
 
 function createTimeBlock() {
     for (var i = 0; i < hourArr.length; i++) {
@@ -33,47 +80,45 @@ function createTimeBlock() {
     };
 };
 
-$(document).on("click", "button", function () {
-
-    var userInput = $(this).parent().find("textarea").val();
-    var textRow = $(this).parent().find("textarea").attr("data-row");
-    console.log(userInput);
-
-    var newEvent = new Object();
-    newEvent.text = userInput
-    newEvent.row = textRow
-    dayPlanner.unshift(newEvent);
-    dayPlanner = removeDuplicates(dayPlanner, "row")
-    dayPlanner.sort(function (x, y) {
-        return x.row - y.row
-    });
-    console.log(dayPlanner);
-
-    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
-
-    function renderEvents() {
-
-        $("textarea").empty;
-
-        var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner"));
 
 
+function renderEvents() {
 
+    var display = JSON.parse(localStorage.getItem("dayPlanner"));
+    console.log(display);
 
-    }
+    $("textarea").empty;
+    for (var i = 0; i < hourArr.length; i++) {
+        console.log("Generating text at iteration" + i)
+        $("#text" + i).text(display[i].text);
+        console.log($("#text" + i).text(display[i].text))
 
-    function removeDuplicates(array, key) {
-        var lookup = {};
-        var result = [];
-        for (var i = 0; i < array.length; i++) {
-            if (!lookup[array[i][key]]) {
-                lookup[array[i][key]] = true;
-                result.push(array[i]);
-            }
+    };
+};
+
+function checkStorage() {
+
+    var storage = JSON.parse(localStorage.getItem("dayPlanner"));
+
+    if (storage === undefined || storage === null) {
+
+        console.log("Day Planner is not in local storage. Creating Object.")
+        storage = [];
+
+        for (var i = 0; i < hourArr.length; i++) {
+
+            var generateObj = new Object();
+            generateObj.currentDay = $("#currentDay").text();
+            generateObj.row = i
+            generateObj.text = ""
+            console.log(generateObj);
+            storage.push(generateObj);
+            localStorage.setItem("dayPlanner", JSON.stringify(storage));
         }
-        return result;
     }
-});
+
+    localStorage.setItem("dayPlanner", JSON.stringify(storage));
+}
 //In the timeblocks, there are 3 parts:
 // Display Hour, Display Time Event, Display Save Button
 
